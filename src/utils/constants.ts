23301be58c8fde0a38359bedba5cc5b3804b802f
AgnieszkaUzarek <azuzarek@gmail.com>
2024-03-25 00:46:42 +0100

    create consts

---
+++ src/utils/constants.ts
@@ -0,0 +1,10 @@
+export const DATA_URL = "https://reqres.in/api/products";
+
+export const numberOfItemsToDisplayPerPage = 5;
+
+export const columns = [{ field: "Id" }, { field: "Name" }, { field: "Year" }];
+
+export const colors = {
+  dark: "#272522",
+  btnColor: "#c4bbab",
+};
