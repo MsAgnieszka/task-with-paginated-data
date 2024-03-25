20aa77d464f9f7656825576ec3e58972b8dac0f2
AgnieszkaUzarek <azuzarek@gmail.com>
2024-03-25 01:10:06 +0100

    implement ModalWithDetails to ListItem

--- src/components/ListWithData/ListItem.tsx
+++ src/components/ListWithData/ListItem.tsx
@@ -1,20 +1,12 @@
-import { Box, Modal } from '@mui/material';
 import { useState } from 'react';
 import { DataType } from '../../screens/MainPage';
+import { ModalWithDetails } from '../ModalWithDetails';
 
 type Props = { data: DataType };
 
 export const ListItem = ({ data }: Props) => {
-	const { color, id, name, year, pantone_value } = data;
+	const { color, id, name, year } = data;
 	const [openModal, setOpenModal] = useState<boolean>(false);
-
-	const details = [
-		{ label: 'Id', value: id },
-		{ label: 'Name', value: name },
-		{ label: 'Color', value: color },
-		{ label: 'Year', value: year },
-		{ label: 'Pantone Value', value: pantone_value },
-	];
 
 	const handleOpen = () => {
 		setOpenModal(true);
@@ -26,42 +18,23 @@ export const ListItem = ({ data }: Props) => {
 	return (
 		<>
 			<tr
+				onClick={handleOpen}
 				style={{
 					backgroundColor: color,
-				}}
-				onClick={handleOpen}>
+				}}>
 				<td style={tdStyle}>{id}</td>
 				<td style={tdStyle}>{name}</td>
 				<td style={tdStyle}>{year}</td>
 			</tr>
-			<Modal open={openModal} onClose={handleClose}>
-				<Box sx={{ ...boxStyle, width: 400 }}>
-					<h2 id='modalTitle'>Item details</h2>
-					{details.map(({ label, value }, i) => (
-						<p
-							key={`${label}-${i}`}
-							id={`modalDetailColor${label.replace(' ', '')}`}>
-							{label}: {value}
-						</p>
-					))}
-				</Box>
-			</Modal>
+			<ModalWithDetails
+				data={data}
+				openModal={openModal}
+				handleClose={handleClose}
+			/>
 		</>
 	);
 };
 
 const tdStyle = {
 	paddingInline: 20,
-};
-
-const boxStyle = {
-	position: 'absolute',
-	top: '50%',
-	left: '50%',
-	transform: 'translate(-50%, -50%)',
-	width: 400,
-	bgcolor: 'background.paper',
-	border: '1px solid #000',
-	boxShadow: 24,
-	p: 4,
 };
