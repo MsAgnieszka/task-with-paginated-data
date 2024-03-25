ae2e71bdfe046655565347c3381c05d2e24fffd2
AgnieszkaUzarek <agnieszka.uzarek@mindconsulting.pl>
2024-03-25 21:25:12 +0100

    changes with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import './App.css';
-
-import { HashRouter, Route, Routes } from 'react-router-dom';
-import { MainPage } from './screens/MainPage';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { MainPage } from "./screens/MainPage";
 
 const App = () => {
-	return (
-		<HashRouter>
-			<Routes>
-				<Route path='/' Component={MainPage} />
-			</Routes>
-		</HashRouter>
-	);
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/?" element={<MainPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
--- src/screens/MainPage.tsx
+++ src/screens/MainPage.tsx
@@ -1,51 +1,55 @@
-import { ChangeEvent, useEffect, useState } from 'react';
-import { ListWithData } from '../components/ListWithData/ListWithData';
-import { SearchBar } from '../components/SearchBar';
-import { useDataFromApi } from '../hooks/useDataFromApi';
-import { Pagination, Stack } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import { ChangeEvent, useEffect, useState } from "react";
+import { ListWithData } from "../components/ListWithData/ListWithData";
+import { SearchBar } from "../components/SearchBar";
+import { useDataFromApi } from "../hooks/useDataFromApi";
+import { Pagination, Stack } from "@mui/material";
+import { useNavigate, useLocation } from "react-router-dom";
+
+const useQuery = () => new URLSearchParams(useLocation().search);
 
 export type DataType = {
-	id: number;
-	name: string;
-	year: number;
-	color: string;
-	pantone_value?: string;
+  id: number;
+  name: string;
+  year: number;
+  color: string;
+  pantone_value?: string;
 };
 
 export const MainPage = () => {
-	const navigate = useNavigate();
-	const { getDataFromApi, items, totalPages, currentPage, setCurrentPage } =
-		useDataFromApi();
-	const [localCurrentPage, setLocalCurrentPage] = useState<number>(1);
+  const navigate = useNavigate();
+  const query = useQuery();
+  const pageFromUrl = query.get("page");
+  const idFromUrl = query.get("id");
 
-	useEffect(() => {
-		setLocalCurrentPage(currentPage);
-		getDataFromApi();
-	}, [currentPage, getDataFromApi]);
+  console.log("pageFromUrl", pageFromUrl);
+  console.log("idFromUrl", idFromUrl);
+  const { getDataFromApi, items, totalPages, currentPage, setCurrentPage } =
+    useDataFromApi();
+  const [localCurrentPage, setLocalCurrentPage] = useState<number>(1);
 
-	const handleChange = (event: ChangeEvent<unknown>, value: number) => {
-		setLocalCurrentPage(value);
-		setCurrentPage(value);
-		navigate(`/?page=${value}`);
-	};
+  useEffect(() => {
+    setLocalCurrentPage(currentPage);
+    getDataFromApi();
+  }, [currentPage, getDataFromApi]);
 
-	return (
-		<div>
-			<div className='App'>
-				<Stack className='App-header' spacing={3}>
-					<SearchBar onSearch={getDataFromApi} page={localCurrentPage} />
-					<ListWithData data={items} />
-					<Pagination
-						count={totalPages}
-						variant='outlined'
-						color='primary'
-						disabled={!Array.isArray(items)}
-						onChange={handleChange}
-						page={localCurrentPage}
-					/>
-				</Stack>
-			</div>
-		</div>
-	);
+  const handleChange = (event: ChangeEvent<unknown>, value: number) => {
+    setLocalCurrentPage(value);
+    setCurrentPage(value);
+    navigate(`/?page=${value}`);
+  };
+
+  return (
+    <Stack className="App-header" spacing={3}>
+      <SearchBar onSearch={getDataFromApi} page={localCurrentPage} />
+      <ListWithData data={items} />
+      <Pagination
+        count={totalPages}
+        variant="outlined"
+        color="primary"
+        disabled={!Array.isArray(items)}
+        onChange={handleChange}
+        page={localCurrentPage}
+      />
+    </Stack>
+  );
 };
