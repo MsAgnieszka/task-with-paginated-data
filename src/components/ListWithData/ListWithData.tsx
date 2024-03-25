24316dea6e2c60c154796abf9d9eb858e319e390
AgnieszkaUzarek <azuzarek@gmail.com>
2024-03-25 07:46:36 +0100

    change styles

--- src/components/ListWithData/ListWithData.tsx
+++ src/components/ListWithData/ListWithData.tsx
@@ -1,22 +1,22 @@
-import { DataType } from "../../screens/MainPage";
-import { ListBody } from "./ListBody";
-import { ListHeader } from "./ListHeader";
+import { DataType } from '../../screens/MainPage';
+import { ListBody } from './ListBody';
+import { ListHeader } from './ListHeader';
 
 type ListWithDataProps = {
-  data: DataType | DataType[];
+	data: DataType | DataType[];
 };
 
 export const ListWithData = ({ data }: ListWithDataProps) => {
-  return (
-    <div className="displayDataContainer">
-      {data ? (
-        <table>
-          <ListHeader />
-          <ListBody data={data} />
-        </table>
-      ) : (
-        <text>No results</text>
-      )}
-    </div>
-  );
+	return (
+		<div className='displayDataContainer' style={{ fontSize: 24 }}>
+			{data ? (
+				<table>
+					<ListHeader />
+					<ListBody data={data} />
+				</table>
+			) : (
+				<text>No results</text>
+			)}
+		</div>
+	);
 };
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,80 +1,81 @@
-import { Button, TextField } from "@mui/material";
-import { ChangeEvent, useCallback, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useDataFromApi } from "../hooks/useDataFromApi";
+import { Box, Button, TextField } from '@mui/material';
+import { ChangeEvent, useCallback, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDataFromApi } from '../hooks/useDataFromApi';
 
 type SearchBarProps = {
-  onSearch: (id?: number) => Promise<void>;
-  page?: number;
+	onSearch: (id?: number) => Promise<void>;
+	page?: number;
 };
 
 export const SearchBar = ({ onSearch, page }: SearchBarProps) => {
-  const navigate = useNavigate();
-  const { currentPage } = useDataFromApi();
-  const [inputContent, setInputContent] = useState<string>("");
+	const navigate = useNavigate();
+	const { currentPage } = useDataFromApi();
+	const [inputContent, setInputContent] = useState<string>('');
 
-  const handleSearch = useCallback(
-    () => onSearch(+inputContent),
-    [inputContent, onSearch]
-  );
+	const handleSearch = useCallback(
+		() => onSearch(+inputContent),
+		[inputContent, onSearch]
+	);
 
-  const handleClear = useCallback(() => {
-    setInputContent("");
-    onSearch().then(() => navigate(`/?page=${page}`));
-  }, [navigate, onSearch, page]);
+	const handleClear = useCallback(() => {
+		setInputContent('');
+		onSearch().then(() => navigate(`/?page=${page}`));
+	}, [navigate, onSearch, page]);
 
-  const changeHandler = ({
-    target: { value },
-  }: ChangeEvent<HTMLInputElement>) => {
-    setInputContent(value);
-  };
+	const changeHandler = ({
+		target: { value },
+	}: ChangeEvent<HTMLInputElement>) => {
+		setInputContent(value);
+	};
 
-  return (
-    <div
-      className="search-container"
-      style={{ flexDirection: "row", height: 40 }}
-    >
-      <TextField
-        type="number"
-        size="small"
-        placeholder="Type id (numbers only)"
-        name="searchInput"
-        value={inputContent}
-        variant="outlined"
-        onChange={changeHandler}
-        style={{ width: 220 }}
-      />
-      <Link
-        to={
-          inputContent
-            ? `/?page=${currentPage}&id=${inputContent}`
-            : `/?page=${page}`
-        }
-      >
-        <Button
-          onClick={handleSearch}
-          size="small"
-          style={btnStyle}
-          variant="contained"
-        >
-          Search
-        </Button>
-        <Button
-          onClick={handleClear}
-          size="small"
-          style={btnStyle}
-          variant="outlined"
-        >
-          Clear
-        </Button>
-      </Link>
-    </div>
-  );
+	return (
+		<Box
+			className='search-container'
+			style={{
+				display: 'flex',
+				alignItems: 'center',
+				flexDirection: 'row',
+			}}>
+			<TextField
+				type='number'
+				size='small'
+				placeholder='Type id (numbers only)'
+				name='searchInput'
+				value={inputContent}
+				variant='outlined'
+				onChange={changeHandler}
+				style={{ width: 220 }}
+			/>
+			<>
+				<Link
+					to={
+						inputContent
+							? `/?page=${currentPage}&id=${inputContent}`
+							: `/?page=${page}`
+					}>
+					<Button
+						onClick={handleSearch}
+						size='medium'
+						style={btnStyle}
+						variant='contained'
+						disableElevation>
+						Search
+					</Button>
+					<Button
+						onClick={handleClear}
+						size='medium'
+						style={btnStyle}
+						variant='outlined'>
+						Clear
+					</Button>
+				</Link>
+			</>{' '}
+		</Box>
+	);
 };
 
 const btnStyle = {
-  marginLeft: 8,
-  color: "black",
-  height: "100%",
-  paddingInline: 8,
+	marginLeft: 8,
+	color: 'black',
 };
